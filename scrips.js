d3d9f0e740ae246e2cc4c7c477ea6a9f242630ad
daniloBlanco <daniloblanco3rob@gmail.com>
2026-02-11 14:19:10 -0400

    Initial commit

---
+++ scrips.js
@@ -0,0 +1,9 @@
+function mostrarRegistro() {
+  document.getElementById("login").classList.remove("activo");
+  document.getElementById("registro").classList.add("activo");
+}
+
+function mostrarLogin() {
+  document.getElementById("registro").classList.remove("activo");
+  document.getElementById("login").classList.add("activo");
+}
